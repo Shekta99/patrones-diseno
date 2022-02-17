db5a8c232aa8d4d95320c6b6e5b0ba34185c42d9
Sergio Marentes <sergiomarentes705@gmail.com>
2022-02-17 08:26:39 -0500

    añadir prueba model X decorator pattern)

--- src/patterns/1-decorator/decorator.test.ts
+++ src/patterns/1-decorator/decorator.test.ts
@@ -1,4 +1,4 @@
-import { EnhancedAutopilot, ModelS } from "./decorator";
+import { EnhancedAutopilot, ModelS, ModelX } from "./decorator";
 
 describe("decorator pattern", () => {
   it("should decorate an tesla model S", () => {
@@ -10,5 +10,16 @@ describe("decorator pattern", () => {
       "Model S, enhanced autopilot"
     );
     expect(myEnhancedTesla.cost()).toEqual(78000);
+  });
+
+  it("should decorate an tesla model X", () => {
+    const myTesla = new ModelX();
+
+    const myEnhancedTesla = new EnhancedAutopilot(myTesla);
+
+    expect(myEnhancedTesla.getDescription()).toEqual(
+      "Model X, enhanced autopilot"
+    );
+    expect(myEnhancedTesla.cost()).toEqual(82000);
   });
 });
